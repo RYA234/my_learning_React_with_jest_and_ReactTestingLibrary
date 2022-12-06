77a17768bb44a3347ec0301158a1a351c3a014a7
RYA234 <103587568+RYA234@users.noreply.github.com>
2022-12-06 04:34:12 +0000

    React testing library is added

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { logRoles } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
    const { container } = render(<App />);
-	logRoles(container);
 
     // find an element with a role of button and text of 'Change to blue'
    const colorButton = screen.getByRole('button',{ name:'Change to blue' });
 
+   //　赤色の色があることを確認
     expect(colorButton).toHaveStyle({backgroundColor: 'red'})
 });
-
-
 
 test('button turns blue when clicked', () => {});
