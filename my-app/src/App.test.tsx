55cc3b9767655734d5a61e5ce5496b0a94d058b5
RYA234 <103587568+RYA234@users.noreply.github.com>
2022-12-06 05:41:54 +0000

    section2 14 RoleLog version

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { logRoles } from '@testing-library/react';
 import App from './App';
 
+
 test('button has correct initial color', () => {
-   const { container } = render(<App />);
+    //コンテナ化する
+	const { container } = render(<App />);
+	logRoles(container);
 
     // find an element with a role of button and text of 'Change to blue'
    const colorButton = screen.getByRole('button',{ name:'Change to blue' });
 
    //　ボタンの初期色が赤色であることを確認
-    expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+    expect(colorButton).toHaveStyle({backgroundColor: 'yellow'})
 });
 
 test('button turns blue when clicked', () => {});
