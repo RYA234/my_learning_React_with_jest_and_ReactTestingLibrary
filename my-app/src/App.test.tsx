70b4b4ffabdc08d651f61622d0c4ace4e761350c
RYA234 <103587568+RYA234@users.noreply.github.com>
2022-12-07 04:56:39 +0000

    section2 15 16 is ended

--- my-app/src/App.test.tsx
+++ my-app/src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 
@@ -9,7 +9,33 @@ test('button has correct initial color', () => {
    const colorButton = screen.getByRole('button',{ name:'Change to blue' });
 
    //　ボタンの初期色が赤色であることを確認
-    expect(colorButton).toHaveStyle({backgroundColor: 'yellow'})
+    expect(colorButton).toHaveStyle({backgroundColor: 'red'})
+
+	// ボタンを押す
+	fireEvent.click(colorButton);
+
+	// クリック後ボタンは青色になる
+	expect(colorButton).toHaveStyle({ backgroundColor:'blue'});
+
+	// クリック後ボタンは赤になる。
+	expect(colorButton).toHaveTextContent('Change to red');
+	
+	// ボタンを押す
+	fireEvent.click(colorButton);
+
+	// クリック後ボタンは青色になる
+	expect(colorButton).toHaveStyle({ backgroundColor:'red'});
+
+	// クリック後ボタンは赤になる。
+	expect(colorButton).toHaveTextContent('Change to blue');
+
+
 });
 
-test('button turns blue when clicked', () => {});
+test('button turns blue when clicked', () => {
+	render(<App />);
+	const colorButton = screen.getByRole('button',{ name:'Change to blue'});
+
+
+
+});
