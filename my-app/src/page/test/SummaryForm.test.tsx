fbdacc83ff4656a2b385d6a1b80b6723e52fe4dc
RYA234 <103587568+RYA234@users.noreply.github.com>
2022-12-14 01:03:01 +0000

    Section4 41 42  Popover added

--- my-app/src/page/test/SummaryForm.test.tsx
+++ my-app/src/page/test/SummaryForm.test.tsx
@@ -1,27 +1,47 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("Initial conditions", () => {
-  render(<SummaryForm />);
-  const checkbox = screen.getByRole("checkbox", {
-    name: /terms and conditions/i,
-  });
-  expect(checkbox).not.toBeChecked();
+	render(<SummaryForm />);
+	const checkbox = screen.getByRole("checkbox", {
+		name: /terms and conditions/i,
+	});
+	expect(checkbox).not.toBeChecked();
 
-  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
-  expect(confirmButton).toBeDisabled();
+	const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+	expect(confirmButton).toBeDisabled();
 });
 
-test("Checkbox enables button on first click and disables on second click", () => {
-  render(<SummaryForm />);
-  const checkbox = screen.getByRole("checkbox", {
-    name: /terms and conditions/i,
-  });
-  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+test("Checkbox enables button on first click and disables on second click", async () => {
+	const user = userEvent.setup();
 
-  fireEvent.click(checkbox);
-  expect(confirmButton).toBeEnabled();
+	render(<SummaryForm />);
+	const checkbox = screen.getByRole("checkbox", {
+		name: /terms and conditions/i,
+	});
+	const confirmButton = screen.getByRole("button", { name: /confirm order/i });
 
-  fireEvent.click(checkbox);
-  expect(confirmButton).toBeDisabled();
+	await user.click(checkbox);
+	expect(confirmButton).toBeEnabled();
+
+	await user.click(checkbox);
+	expect(confirmButton).toBeDisabled();
+});
+
+
+test("popover responds to hover", async () => {
+	const user = userEvent.setup();
+
+	render(<SummaryForm />);
+	const checkbox = screen.getByRole("checkbox", {
+		name: /terms and conditions/i,
+	});
+	const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+
+	await user.click(checkbox);
+	expect(confirmButton).toBeEnabled();
+
+	await user.click(checkbox);
+	expect(confirmButton).toBeDisabled();
 });
